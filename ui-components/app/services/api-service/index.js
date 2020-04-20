59c27c98b2e09d8f75ce680ff1ae79f227a91c88
bohdan-meltiukhov <bohdan.meltiukhov@teamdev.com>
2020-04-20 11:44:53 +0300

    Write tests for the ApiService using fetch-mock

--- ui-components/app/services/api-service/index.js
+++ ui-components/app/services/api-service/index.js
@@ -2,8 +2,8 @@ import UserCredentials from '../../models/user-credentials';
 import ValidationErrorCase from '../../models/errors/validation-error-case';
 import AuthorizationError from '../../models/errors/authorization-error';
 import ServerValidationError from '../../models/errors/server-validation-error';
-import fetchMock from '../../../node_modules/fetch-mock/esm/client.js';
 import GeneralServerError from '../../models/errors/general-server-error';
+import FetchMock from '../fetch-mock';
 
 let instance;
 
@@ -14,37 +14,12 @@ export default class ApiService {
   /**
    * Creates an instance of the API service with set fetch mock.
    *
-   * @param {Function} fetchFunction - The fetch function to use.
+   * @private
    */
-  constructor(fetchFunction) {
-    this._fetch = fetchFunction.bind(window);
-
-    fetchMock.post('/login', (url, options) => {
-      const credentials = options.body;
-      if (credentials.username === 'admin' && credentials.password === '1234') {
-        return {token: 'my-token'};
-      }
-      return 401;
-    }, {overwriteRoutes: false});
-
-    fetchMock.post('/register', (url, options) => {
-      const credentials = options.body;
-      if (credentials.username === 'admin') {
-        return {
-          status: 422,
-          body: {
-            errors: [
-              {
-                field: 'username',
-                message: 'The username is already taken.',
-              },
-            ],
-          },
-        };
-      } else {
-        return 200;
-      }
-    }, {overwriteRoutes: false});
+  constructor() {
+    if (window.devMode) {
+      FetchMock.setMock();
+    }
   }
 
   /**
@@ -54,7 +29,7 @@ export default class ApiService {
    * @returns {Promise} The promise that resolves when a response from the server is received.
    */
   logIn(userCredentials) {
-    return this._fetch('/login', {
+    return fetch('/login', {
       method: 'POST',
       body: userCredentials,
     })
@@ -72,7 +47,7 @@ export default class ApiService {
    * @returns {Promise} The promise that resolves when a response from the server is received.
    */
   register(userCredentials) {
-    return this._fetch('/register', {
+    return fetch('/register', {
       method: 'POST',
       body: userCredentials,
     })
@@ -92,7 +67,7 @@ export default class ApiService {
    */
   _handleAuthenticationErrors(response) {
     let error;
-    console.log('response.status', response.status);
+
     switch (response.status) {
     case 401:
       error = new AuthorizationError('The username or password is incorrect');
@@ -122,7 +97,7 @@ export default class ApiService {
    */
   static getInstance() {
     if (!instance) {
-      instance = new this(fetch);
+      instance = new this();
     }
     return instance;
   }
--- ui-components/tests/services/api-service/index.js
+++ ui-components/tests/services/api-service/index.js
@@ -1,48 +1,54 @@
 import ApiService from '../../../app/services/api-service';
 import UserCredentials from '../../../app/models/user-credentials';
+import fetchMock from '../../../node_modules/fetch-mock/esm/client.js';
+
 
 const {module, test} = QUnit;
 
 module('The ApiService');
 
 test('should log in.', (assert) => {
-  const fetchMock = (url, options) => {
-    return new Promise((resolve) => {
-      if (url === '/login' && options.method === 'POST') {
-        assert.step('Log in fetch called.');
-      }
-      const response = {
-        status: 200,
-      };
-      resolve(response);
-    });
-  };
+  const username = 'admin';
+  const password = '1234';
 
-  const apiService = new ApiService(fetchMock);
-  const userCredentials = new UserCredentials('admin', '1234');
+  fetchMock.post('/login', (url, opts) => {
+    const credentials = opts.body;
+    assert.strictEqual(credentials.username, username, 'The API Service should provide the correct username in the ' +
+      'request body.');
+    assert.strictEqual(credentials.password, password, 'The API Service should provide the correct password in the ' +
+      'request body.');
+    return 200;
+  });
+
+  const apiService = ApiService.getInstance();
+  const userCredentials = new UserCredentials(username, password);
 
   apiService.logIn(userCredentials);
 
-  assert.verifySteps(['Log in fetch called.'], 'The API service should call the fetch function.');
+  assert.ok(fetchMock.called('/login', {
+    method: 'POST',
+  }), 'The API Service should send the POST request to the \'/login\' URL.');
 });
 
 test('should register.', (assert) => {
-  const fetchMock = (url, options) => {
-    return new Promise((resolve) => {
-      if (url === '/register' && options.method === 'POST') {
-        assert.step('Register fetch called.');
-      }
-      const response = {
-        status: 200,
-      };
-      resolve(response);
-    });
-  };
+  const username = 'admin';
+  const password = '1234';
 
-  const apiService = new ApiService(fetchMock);
-  const userCredentials = new UserCredentials('user', 'password1234');
+  fetchMock.post('/register', (url, opts) => {
+    const credentials = opts.body;
+    assert.strictEqual(credentials.username, username, 'The API Service should provide the correct username in the ' +
+      'request body.');
+    assert.strictEqual(credentials.password, password, 'The API Service should provide the correct password in the ' +
+      'request body.');
+    return 200;
+  });
+
+  const apiService = ApiService.getInstance();
+  const userCredentials = new UserCredentials(username, password);
 
   apiService.register(userCredentials);
 
-  assert.verifySteps(['Register fetch called.'], 'The API service should call the fetch function.');
+  assert.ok(fetchMock.called('/register', {
+    method: 'POST',
+  }), 'The API Service should send the POST request to the \'/register\' URL.');
 });

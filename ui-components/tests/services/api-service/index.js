0728431098830422c39a5fa373a5128a78a0c401
bohdan-meltiukhov <bohdan.meltiukhov@teamdev.com>
2020-04-24 09:59:20 +0300

    Add tests for the API Service

--- ui-components/tests/services/api-service/index.js
+++ ui-components/tests/services/api-service/index.js
@@ -52,3 +52,58 @@ test('should register.', (assert) => {
     method: 'POST',
   }), 'The API Service should send the POST request to the \'/register\' URL.');
 });
+
+test('should update folders.', (assert) => {
+  assert.expect(3);
+
+  const folder = {
+    id: 'uExvhDL4YwkxnBVa',
+    parentId: '4Goz0J0Tz8xfDfsJ',
+    name: 'Documents',
+    itemsNumber: 20,
+    type: 'folder',
+  };
+
+  fetchMock.put('glob:/folder/*', (url, opts) => {
+    const id = url.slice(8);
+
+    assert.strictEqual(id, folder.id, 'The updateFolder() method should send a request with correct folder id.');
+    assert.deepEqual(opts.body.element, folder, 'The updateFolder() method should send a request with correct ' +
+      'folder object.');
+  });
+
+  const apiService = ApiService.getInstance();
+  apiService.updateFolder(folder);
+
+  assert.ok(fetchMock.called('glob:/folder/*', {
+    method: 'PUT',
+  }), 'The updateFolder() method should send a PUT request to the \'/folder/:id\' URL.');
+});
+
+test('should update files.', (assert) => {
+  assert.expect(3);
+
+  const file = {
+    id: 'ARqTPQ1XXUrFlaJe',
+    parentId: 'tRZXiSHNRlgZluGQ',
+    name: 'Montenegro.jpg',
+    mimeType: 'image',
+    size: 162,
+    type: 'file',
+  };
+
+  fetchMock.put('glob:/file/*', (url, opts) => {
+    const id = url.slice(6);
+
+    assert.strictEqual(id, file.id, 'The updateFile() method should send a request with correct file id.');
+    assert.deepEqual(opts.body.element, file, 'The updateFile() method should send a request with correct ' +
+      'file object.');
+  });
+
+  const apiService = ApiService.getInstance();
+  apiService.updateFile(file);
+
+  assert.ok(fetchMock.called('glob:/file/*', {
+    method: 'PUT',
+  }), 'The updateFile() method should send a PUT request to the \'/file/:id\' URL.');
+});

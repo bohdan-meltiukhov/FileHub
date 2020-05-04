2aff9ae3e3c191f2dff9495bf9662cf749900ce2
bohdan-meltiukhov <bohdan.meltiukhov@teamdev.com>
2020-05-04 12:50:41 +0300

    Write a test for the DownloadedFileMutator

--- ui-components/tests/test-index.js
+++ ui-components/tests/test-index.js
@@ -25,3 +25,4 @@ import * as folderMutator from './state/mutators/folder-mutator';
 import * as folderLoadingErrorMutator from './state/mutators/folder-loading-error-mutator';
 import * as isFolderLoadingMutator from './state/mutators/is-folder-loading-mutator';
 import * as downloadFileAction from './state/actions/download-file-action';
+import * as downloadedFileMutator from './state/mutators/downloaded-file-mutator';

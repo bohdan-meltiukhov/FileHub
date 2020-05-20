92eb3c0890fffdc06c95e2d11ad815ce0d587c71
bohdan-meltiukhov <bohdan.meltiukhov@teamdev.com>
2020-05-20 08:13:08 +0300

    Edit the GetUserAction test

--- ui-components/tests/state/actions/get-user-action/index.js
+++ ui-components/tests/state/actions/get-user-action/index.js
@@ -1,12 +1,13 @@
 import UserNameMutator from '../../../../app/state/mutators/user-name-mutator';
 import GetUserAction from '../../../../app/state/actions/get-user-action';
+import IsUserNameLoadingMutator from '../../../../app/state/mutators/is-user-name-loading-mutator';
 
 const {module, test} = QUnit;
 
 module('The GetUserAction');
 
 test('should get the current user.', async (assert) => {
-  assert.expect(2);
+  assert.expect(8);
 
   const user = {
     name: 'John',
@@ -14,6 +15,7 @@ test('should get the current user.', async (assert) => {
 
   const getUser = () => {
     return new Promise((resolve) => {
+      assert.ok(true, 'The GetUserAction should call the ApiService.getUser() method.');
       resolve(user);
     });
   };
@@ -24,13 +26,24 @@ test('should get the current user.', async (assert) => {
 
   const stateManagerMock = {
     mutate: (mutator) => {
-      assert.ok(mutator instanceof UserNameMutator, 'The GetUserAction should provide an instance of the ' +
-        'UserNameMutator to the state manager.');
-      assert.strictEqual(mutator._name, user.name, 'The GetUserAction should provide correct user name to the ' +
-        'UserNameMutator.');
+      assert.step(mutator.constructor.name);
+      if (mutator instanceof IsUserNameLoadingMutator) {
+        assert.step(`IsLoading: ${mutator._isLoading}.`);
+      } else if (mutator instanceof UserNameMutator) {
+        assert.strictEqual(mutator._name, user.name, 'The GetUserAction should provide correct user name to the ' +
+          'UserNameMutator.');
+      }
     },
   };
 
   const action = new GetUserAction();
-  action.apply(stateManagerMock, apiServiceMock);
+  await action.apply(stateManagerMock, apiServiceMock);
+
+  assert.verifySteps([
+    'IsUserNameLoadingMutator',
+    'IsLoading: true.',
+    'UserNameMutator',
+    'IsUserNameLoadingMutator',
+    'IsLoading: false.',
+  ], 'The GetUserAction should provide mutators to the state manager in the correct order.');
 });

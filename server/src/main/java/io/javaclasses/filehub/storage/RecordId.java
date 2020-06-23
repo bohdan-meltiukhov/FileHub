daf8710e862108f43728c7112126aefe0e508cff
Bohdan Meltiukhov <bohdan.meltiukhov@teamdev.com>
2020-06-23 09:42:05 +0300

    Add the String value() method to the RecordId interface

--- server/src/main/java/io/javaclasses/filehub/storage/RecordId.java
+++ server/src/main/java/io/javaclasses/filehub/storage/RecordId.java
@@ -4,4 +4,11 @@ package io.javaclasses.filehub.storage;
  * The identifier for different storage records.
  */
 public interface RecordId {
+
+    /**
+     * Provides the identifier value.
+     *
+     * @return The value of an identifier.
+     */
+    String value();
 }

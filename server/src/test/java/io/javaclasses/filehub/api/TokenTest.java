aca733507d7f6b9c19392a3aaedaa087f1b1f393
bohdan-meltiukhov <bohdan.meltiukhov@teamdev.com>
2020-06-30 19:23:34 +0300

    Add the Token class

---
+++ server/src/test/java/io/javaclasses/filehub/api/TokenTest.java
@@ -0,0 +1,29 @@
+package io.javaclasses.filehub.api;
+
+import com.google.common.testing.NullPointerTester;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static nl.jqno.equalsverifier.EqualsVerifier.forClass;
+import static nl.jqno.equalsverifier.Warning.NULL_FIELDS;
+
+class TokenTest {
+
+    @Test
+    @DisplayName("not accept null values.")
+    void testNullPointers() {
+        NullPointerTester tester = new NullPointerTester();
+
+        tester.testAllPublicConstructors(Token.class);
+        tester.testAllPublicInstanceMethods(new Token(""));
+    }
+
+    @Test
+    @DisplayName("fulfill the equals() and hashCode() contract.")
+    void testEqualsContract() {
+
+        forClass(Token.class)
+                .suppress(NULL_FIELDS)
+                .verify();
+    }
+}

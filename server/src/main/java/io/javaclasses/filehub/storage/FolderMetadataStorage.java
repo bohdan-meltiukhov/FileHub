2c25b0d17d525f0e61c54a4cb7cdfee76959de78
bohdan-meltiukhov <bohdan.meltiukhov@teamdev.com>
2020-07-01 13:43:16 +0300

    Ad the FolderMetadataStorage

---
+++ server/src/main/java/io/javaclasses/filehub/storage/FolderMetadataStorage.java
@@ -0,0 +1,7 @@
+package io.javaclasses.filehub.storage;
+
+/**
+ * An in-memory {@link Storage} for {@link FolderMetadataRecord}.
+ */
+public class FolderMetadataStorage extends InMemoryStorage<FolderId, FolderMetadataRecord> {
+}

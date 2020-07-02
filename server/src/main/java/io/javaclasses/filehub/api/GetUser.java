041fe05a8a41b0ff817a5bd59d9b64ecf7cdc85e
bohdan-meltiukhov <bohdan.meltiukhov@teamdev.com>
2020-07-02 13:58:59 +0300

    Add the GetUser command

---
+++ server/src/main/java/io/javaclasses/filehub/api/GetUser.java
@@ -0,0 +1,37 @@
+package io.javaclasses.filehub.api;
+
+import com.google.errorprone.annotations.Immutable;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * The command that represents intention of a client to get the current user.
+ */
+@Immutable
+public class GetUser implements Command {
+
+    /**
+     * The provided authentication token.
+     */
+    private final Token token;
+
+    /**
+     * Creates an instance of the GetUser command.
+     *
+     * @param token The authentication token from the client.
+     */
+    public GetUser(Token token) {
+
+        this.token = checkNotNull(token);
+    }
+
+    /**
+     * Provides the command token.
+     *
+     * @return The stored authentication token.
+     */
+    public Token token() {
+
+        return token;
+    }
+}

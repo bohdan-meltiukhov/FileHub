57db3ca432dbfa60aa3e306a10e543d0a14d43c9
bohdan-meltiukhov <bohdan.meltiukhov@teamdev.com>
2020-07-07 11:54:50 +0300

    Edit the UserView documentation

--- server/src/main/java/io/javaclasses/filehub/api/UserView.java
+++ server/src/main/java/io/javaclasses/filehub/api/UserView.java
@@ -5,12 +5,12 @@ import io.javaclasses.filehub.storage.UserRecord;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
- * An {@link ApplicationProcess} that provides the current user.
+ * An {@link ApplicationProcess} that handles the {@link GetUser} query.
  */
 public class UserView implements View<GetUser, UserRecord> {
 
     /**
-     * Checks the {@link GetUser} query and provides the current user.
+     * Processes the {@link GetUser} query and provides the current user.
      *
      * @param query The query from the client
      * @return The current user.

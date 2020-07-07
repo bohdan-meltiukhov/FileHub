f0550cad1d8a7f0055f2eeeaec3c185db31a2e06
bohdan-meltiukhov <bohdan.meltiukhov@teamdev.com>
2020-07-07 12:22:10 +0300

    Add the FileMetadataRecord

---
+++ server/src/main/java/io/javaclasses/filehub/storage/FileMetadataRecord.java
@@ -0,0 +1,8 @@
+package io.javaclasses.filehub.storage;
+
+public class FileMetadataRecord implements StorageRecord<FileId> {
+    @Override
+    public FileId id() {
+        return null;
+    }
+}

24aac51837c945f190a136849a4481bc82bc220b
bohdan-meltiukhov <bohdan.meltiukhov@teamdev.com>
2020-07-07 13:42:28 +0300

    Document the FileMetadataStorage

--- server/src/main/java/io/javaclasses/filehub/storage/FileMetadataStorage.java
+++ server/src/main/java/io/javaclasses/filehub/storage/FileMetadataStorage.java
@@ -1,4 +1,7 @@
 package io.javaclasses.filehub.storage;
 
+/**
+ * A {@link Storage} with {@link FileMetadataRecord}s.
+ */
 public class FileMetadataStorage extends InMemoryStorage<FileId, FileMetadataRecord> {
 }

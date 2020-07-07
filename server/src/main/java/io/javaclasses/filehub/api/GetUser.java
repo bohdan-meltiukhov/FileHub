e9cc28e5e7db28e0dd0e1a17f1b771e665562f34
bohdan-meltiukhov <bohdan.meltiukhov@teamdev.com>
2020-07-07 11:22:52 +0300

    Make the GetUser query final

--- server/src/main/java/io/javaclasses/filehub/api/GetUser.java
+++ server/src/main/java/io/javaclasses/filehub/api/GetUser.java
@@ -7,7 +7,7 @@ import io.javaclasses.filehub.storage.UserRecord;
  * A {@link Query} that represents intention of a client to get the current user.
  */
 @Immutable
-public class GetUser implements Query {
+public final class GetUser implements Query {
 
     /**
      * Provides the current user.

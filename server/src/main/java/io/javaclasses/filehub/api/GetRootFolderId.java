56c3e13a97de6c23e059f14c05f91693173b36dd
bohdan-meltiukhov <bohdan.meltiukhov@teamdev.com>
2020-07-09 13:23:28 +0300

    Add an AuthenticatedQuery class

--- server/src/main/java/io/javaclasses/filehub/api/GetRootFolderId.java
+++ server/src/main/java/io/javaclasses/filehub/api/GetRootFolderId.java
@@ -3,6 +3,6 @@ package io.javaclasses.filehub.api;
 /**
  * A {@link Query} that represents intention of a client to get the identifier of the root folder.
  */
-public class GetRootFolderId extends Query {
+public class GetRootFolderId extends AuthenticatedQuery {
 
 }
--- server/src/main/java/io/javaclasses/filehub/api/Query.java
+++ server/src/main/java/io/javaclasses/filehub/api/Query.java
@@ -1,26 +1,7 @@
 package io.javaclasses.filehub.api;
 
-import io.javaclasses.filehub.storage.UserRecord;
-
 /**
  * An abstract base which represents intention of a client to retrieve any data from the FileHub application.
  */
-public abstract class Query {
-
-    /**
-     * Provides the current user of the FileHub application.
-     *
-     * @return The current user.
-     * @throws CurrentUserNotSetException in case the current user is unknown.
-     */
-    public UserRecord currentUser() {
-
-        UserRecord currentUser = CurrentUser.get();
-        if (currentUser == null) {
-
-            throw new CurrentUserNotSetException("The current user was not set by the Authentication Filter.");
-        }
-
-        return currentUser;
-    }
+public interface Query {
 }

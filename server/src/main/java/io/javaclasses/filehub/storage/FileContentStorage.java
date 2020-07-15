95c142488c58f0d03ccf4e7d3c3b0d22f5d52baf
bohdan-meltiukhov <bohdan.meltiukhov@teamdev.com>
2020-07-15 11:07:57 +0300

    Add the FileContentStorage

---
+++ server/src/main/java/io/javaclasses/filehub/storage/FileContentStorage.java
@@ -0,0 +1,7 @@
+package io.javaclasses.filehub.storage;
+
+/**
+ * A {@link Storage} with {@link FileContentRecord}s.
+ */
+public class FileContentStorage extends InMemoryStorage<FileId, FileContentRecord> {
+}
